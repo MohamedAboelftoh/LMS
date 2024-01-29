38b12f4dad215797b67e576d1fedc88447b6cf36
MOHAMED <aboelftohm1@gmail.com>
2024-01-29 18:52:41 +0200

    init commit

---
+++ app/src/main/java/com/example/lms/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lms.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lms.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
