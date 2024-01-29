a5ca5d6762c3a5b8795fa21ebb7758df7cddb0ea
MOHAMED <aboelftohm1@gmail.com>
2024-01-29 22:15:43 +0200

    + work on activities splashes

---
+++ app/src/main/java/com/example/lms/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lms.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lms.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
