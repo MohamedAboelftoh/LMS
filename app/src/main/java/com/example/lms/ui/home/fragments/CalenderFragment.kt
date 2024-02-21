8633741f7f5e3a9907a536f493fb8b9d538b2145
Mo.Sh <mohamedragb01097434995@gmail.com>
2024-02-21 14:25:00 +0200

    +Add Calender to CalenderFragment

--- app/src/main/java/com/example/lms/ui/home/fragments/CalenderFragment.kt
+++ app/src/main/java/com/example/lms/ui/home/fragments/CalenderFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.lms.R
+import com.example.lms.databinding.FragmentCalenderBinding
 
 
 class CalenderFragment : Fragment() {
+    lateinit var viewBinding:FragmentCalenderBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calender, container, false)
+        viewBinding=FragmentCalenderBinding.inflate(inflater,container,false)
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
