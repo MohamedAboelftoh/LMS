fe14f3a76c0d665689c59dcea15cf360f240a799
MOHAMED <aboelftohm1@gmail.com>
2024-02-22 21:36:58 +0200

    +Update ApiManager class to solve Exception in server(chain validation failed)

--- app/src/main/java/com/example/lms/ui/api/module/ApiManager.kt
+++ app/src/main/java/com/example/lms/ui/api/module/ApiManager.kt
@@ -1,23 +1,55 @@
 package com.example.lms.ui.api.module
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class ApiManager {
-    companion object{
-        private var retrofit : Retrofit?= null
-        private fun getInstance() : Retrofit {
-            if(retrofit == null)
-            {
-                retrofit = Retrofit.Builder()
-                    .baseUrl("https://nabilramadan.bsite.net")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build()
-            }
-            return retrofit!!
+    companion object {
+        private var retrofit: Retrofit? = null
+
+        init {
+            setupRetrofit()
         }
-        fun getApi():UserServices{
-            return getInstance().create(UserServices::class.java)
+
+        private fun setupRetrofit() {
+            // Create a custom TrustManager that accepts all certificates
+            val trustAllCertificates: Array<TrustManager> = arrayOf(
+                object : X509TrustManager {
+                    override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                    override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                }
+            )
+
+            // Set up SSL context to use the custom TrustManager
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCertificates, java.security.SecureRandom())
+
+            // Create an OkHttpClient with the custom SSL context
+            val okHttpClient = OkHttpClient.Builder()
+                .sslSocketFactory(sslContext.socketFactory, trustAllCertificates[0] as X509TrustManager)
+                .hostnameVerifier { _, _ -> true } // Accept all hostnames
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY // Add logging interceptor
+                })
+                .build()
+
+            // Create Retrofit instance using the custom OkHttpClient
+            retrofit = Retrofit.Builder()
+                .baseUrl("https://nabilramadan.bsite.net")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        fun getApi(): UserServices {
+            return retrofit!!.create(UserServices::class.java)
         }
     }
 }
